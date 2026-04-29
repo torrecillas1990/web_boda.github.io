b22deb5e0adeb32ac49c10751739ac865c7b9089
torrecillas1990 <101094594+torrecillas1990@users.noreply.github.com>
2026-04-29 13:13:14 +0200

    .
    .

--- deportiva/contador-calorias/diario/VS_torrecillas1990_20260428.js
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260428.js
@@ -54,5 +54,45 @@ window.VS_torrecillas1990_20260428 = [
         "prot": 2.34,
         "grasa": 0,
         "carb": 49.725
+    },
+    {
+        "id": 1777460962590,
+        "nombre": "Boquerones en Vinagre (Escurrido) (15g/ml)",
+        "kcal": 23.4,
+        "prot": 3.705,
+        "grasa": 0.9,
+        "carb": 0.105
+    },
+    {
+        "id": 1777460974704,
+        "nombre": "Huevos duros (50g/ml)",
+        "kcal": 60.5,
+        "prot": 6.5,
+        "grasa": 3.85,
+        "carb": 0.25
+    },
+    {
+        "id": 1777460996965,
+        "nombre": "Queso Curado Mezcla (80g/ml)",
+        "kcal": 344.8,
+        "prot": 20,
+        "grasa": 28.8,
+        "carb": 1.44
+    },
+    {
+        "id": 1777461036689,
+        "nombre": "Tortitas de Maiz (30g/ml)",
+        "kcal": 110.4,
+        "prot": 2.25,
+        "grasa": 0.54,
+        "carb": 24
+    },
+    {
+        "id": 1777461045750,
+        "nombre": "Nispero (120g/ml)",
+        "kcal": 56.4,
+        "prot": 0.48,
+        "grasa": 0.24,
+        "carb": 14.4
     }
 ];
---
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260429.js
@@ -0,0 +1,34 @@
+window.VS_torrecillas1990_20260429 = [
+    {
+        "id": 1777461085657,
+        "nombre": "Leche Semidesnatada (1 vaso 250g) (250g/ml)",
+        "kcal": 115,
+        "prot": 8,
+        "grasa": 3.75,
+        "carb": 12
+    },
+    {
+        "id": 1777461096636,
+        "nombre": "Naranja 1 pieza son 150 gramos (600g/ml)",
+        "kcal": 360,
+        "prot": 5.64,
+        "grasa": 0.72,
+        "carb": 70.5
+    },
+    {
+        "id": 1777461115536,
+        "nombre": "Atún Claro al Natural (70g/ml)",
+        "kcal": 68.6,
+        "prot": 14.7,
+        "grasa": 0.84,
+        "carb": 0.63
+    },
+    {
+        "id": 1777461142047,
+        "nombre": "Tortitas de Maiz (15g/ml)",
+        "kcal": 55.2,
+        "prot": 1.125,
+        "grasa": 0.27,
+        "carb": 12
+    }
+];

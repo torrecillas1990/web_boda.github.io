cfb4dbf0530ae1c2fda62fb5b469d523bde9d142
torrecillas1990 <101094594+torrecillas1990@users.noreply.github.com>
2026-05-13 16:59:48 +0200

    .
    .

--- deportiva/contador-calorias/diario/VS_torrecillas1990_20260512.js
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260512.js
@@ -14,5 +14,109 @@ window.VS_torrecillas1990_20260512 = [
         "prot": 3.825,
         "grasa": 2.4,
         "carb": 2.445
+    },
+    {
+        "id": 1778586622574,
+        "nombre": "Tortitas de Maiz (30g/ml)",
+        "kcal": 110.4,
+        "prot": 2.25,
+        "grasa": 0.54,
+        "carb": 24
+    },
+    {
+        "id": 1778586634073,
+        "nombre": "Atún Claro en Aceite de Oliva (80g/ml)",
+        "kcal": 196,
+        "prot": 16.8,
+        "grasa": 14.4,
+        "carb": 0.72
+    },
+    {
+        "id": 1778586653503,
+        "nombre": "Huevos duros (100g/ml)",
+        "kcal": 121,
+        "prot": 13,
+        "grasa": 7.7,
+        "carb": 0.5
+    },
+    {
+        "id": 1778586723537,
+        "nombre": "Energy Drink Full Tropic (500g/ml)",
+        "kcal": 20.5,
+        "prot": 0,
+        "grasa": 0,
+        "carb": 2
+    },
+    {
+        "id": 1778586747678,
+        "nombre": "Kéfir Natural 0% (250g/ml)",
+        "kcal": 90,
+        "prot": 9.75,
+        "grasa": 1.25,
+        "carb": 12
+    },
+    {
+        "id": 1778586780002,
+        "nombre": "Kiwi Gold (230g/ml)",
+        "kcal": 124.2,
+        "prot": 2.07,
+        "grasa": 0.69,
+        "carb": 25.53
+    },
+    {
+        "id": 1778658253146,
+        "nombre": "Kiwi Gold (115g/ml)",
+        "kcal": 62.1,
+        "prot": 1.035,
+        "grasa": 0.345,
+        "carb": 12.765
+    },
+    {
+        "id": 1778658275218,
+        "nombre": "Pan 100% Integral Centeno (100g/ml)",
+        "kcal": 230,
+        "prot": 8,
+        "grasa": 2,
+        "carb": 40
+    },
+    {
+        "id": 1778658299519,
+        "nombre": "Boquerones en Vinagre (Escurrido) (21g/ml)",
+        "kcal": 32.76,
+        "prot": 5.186999999999999,
+        "grasa": 1.26,
+        "carb": 0.147
+    },
+    {
+        "id": 1778658315846,
+        "nombre": "Queso Curado Mezcla (100g/ml)",
+        "kcal": 431,
+        "prot": 25,
+        "grasa": 36,
+        "carb": 1.8
+    },
+    {
+        "id": 1778658347607,
+        "nombre": "Lomo de cebo ibérico 50% raza ibérica (15g/ml)",
+        "kcal": 44.7,
+        "prot": 5.865,
+        "grasa": 2.325,
+        "carb": 0.105
+    },
+    {
+        "id": 1778658357666,
+        "nombre": "Cerveza (350g/ml)",
+        "kcal": 150.5,
+        "prot": 1.61,
+        "grasa": 0,
+        "carb": 12.425
+    },
+    {
+        "id": 1778658367452,
+        "nombre": "Nispero (180g/ml)",
+        "kcal": 84.6,
+        "prot": 0.72,
+        "grasa": 0.36,
+        "carb": 21.6
     }
 ];
---
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260513.js
@@ -0,0 +1,66 @@
+window.VS_torrecillas1990_20260513 = [
+    {
+        "id": 1778658400173,
+        "nombre": "Leche Proteinas (250g/ml)",
+        "kcal": 110,
+        "prot": 15,
+        "grasa": 0.5,
+        "carb": 11.5
+    },
+    {
+        "id": 1778658409633,
+        "nombre": "Cacao puro en polvo (15g/ml)",
+        "kcal": 56.25,
+        "prot": 3.825,
+        "grasa": 2.4,
+        "carb": 2.445
+    },
+    {
+        "id": 1778673679783,
+        "nombre": "Kiwi Gold (230g/ml)",
+        "kcal": 124.2,
+        "prot": 2.07,
+        "grasa": 0.69,
+        "carb": 25.53
+    },
+    {
+        "id": 1778673704796,
+        "nombre": "Pollo Asado (al As) (250g/ml)",
+        "kcal": 592.5,
+        "prot": 67.5,
+        "grasa": 33.75,
+        "carb": 3
+    },
+    {
+        "id": 1778673754437,
+        "nombre": "Pan 100% Integral Centeno (40g/ml)",
+        "kcal": 92,
+        "prot": 3.2,
+        "grasa": 0.8,
+        "carb": 16
+    },
+    {
+        "id": 1778680091752,
+        "nombre": "Vino tinto (250g/ml)",
+        "kcal": 210,
+        "prot": 0.175,
+        "grasa": 0,
+        "carb": 6.8
+    },
+    {
+        "id": 1778680100219,
+        "nombre": "Gominola Gummy (25g/ml)",
+        "kcal": 81.25,
+        "prot": 0.9,
+        "grasa": 0,
+        "carb": 19.125
+    },
+    {
+        "id": 1778684358129,
+        "nombre": "Naranja 1 pieza son 150 gramos (200g/ml)",
+        "kcal": 120,
+        "prot": 1.88,
+        "grasa": 0.24,
+        "carb": 23.5
+    }
+];

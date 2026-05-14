76877d6be14645ac03d67fd6c7aa8aa3deca9f87
torrecillas1990 <101094594+torrecillas1990@users.noreply.github.com>
2026-05-14 12:50:05 +0200

    .
    .

---
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260514.js
@@ -0,0 +1,34 @@
+window.VS_torrecillas1990_20260514 = [
+    {
+        "id": 1778744276597,
+        "nombre": "Leche Proteinas (250g/ml)",
+        "kcal": 110,
+        "prot": 15,
+        "grasa": 0.5,
+        "carb": 11.5
+    },
+    {
+        "id": 1778744281834,
+        "nombre": "Cacao puro en polvo (15g/ml)",
+        "kcal": 56.25,
+        "prot": 3.825,
+        "grasa": 2.4,
+        "carb": 2.445
+    },
+    {
+        "id": 1778755701115,
+        "nombre": "Kiwi Gold (230g/ml)",
+        "kcal": 124.2,
+        "prot": 2.07,
+        "grasa": 0.69,
+        "carb": 25.53
+    },
+    {
+        "id": 1778755754943,
+        "nombre": "Tortilla Patata Calabacin (325g/ml)",
+        "kcal": 477.75,
+        "prot": 17.875,
+        "grasa": 27.3,
+        "carb": 39
+    }
+];

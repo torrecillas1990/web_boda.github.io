c9d10f8483166febbc7e6e3d05befecf7d870871
torrecillas1990 <101094594+torrecillas1990@users.noreply.github.com>
2026-05-15 15:33:57 +0200

    .
    .

--- deportiva/contador-calorias/diario/VS_torrecillas1990_20260515.js
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260515.js
@@ -14,5 +14,29 @@ window.VS_torrecillas1990_20260515 = [
         "prot": 3.825,
         "grasa": 2.4,
         "carb": 2.445
+    },
+    {
+        "id": 1778851830610,
+        "nombre": "Kiwi Gold (230g/ml)",
+        "kcal": 124.2,
+        "prot": 2.07,
+        "grasa": 0.69,
+        "carb": 25.53
+    },
+    {
+        "id": 1778851851736,
+        "nombre": "Tortilla Patata Calabacin (325g/ml)",
+        "kcal": 477.75,
+        "prot": 17.875,
+        "grasa": 27.3,
+        "carb": 39
+    },
+    {
+        "id": 1778851877267,
+        "nombre": "Queso Fresco de Cabra (63g/ml)",
+        "kcal": 271.53,
+        "prot": 8.001,
+        "grasa": 13.796999999999999,
+        "carb": 2.142
     }
 ];

30ba66506d8da8cb0272660b6c312453597554f3
torrecillas1990 <101094594+torrecillas1990@users.noreply.github.com>
2026-05-18 14:56:42 +0200

    .
    .

---
+++ deportiva/contador-calorias/diario/VS_torrecillas1990_20260518.js
@@ -0,0 +1,42 @@
+window.VS_torrecillas1990_20260518 = [
+    {
+        "id": 1779096223226.4543,
+        "nombre": "Leche Proteinas (250g/ml)",
+        "kcal": 110,
+        "prot": 15,
+        "grasa": 0.5,
+        "carb": 11.5
+    },
+    {
+        "id": 1779096223226.2197,
+        "nombre": "Cacao puro en polvo (15g/ml)",
+        "kcal": 56.25,
+        "prot": 3.825,
+        "grasa": 2.4,
+        "carb": 2.445
+    },
+    {
+        "id": 1779108909296,
+        "nombre": "Tortilla Patata Calabacin (215g/ml)",
+        "kcal": 316.05,
+        "prot": 11.825,
+        "grasa": 18.06,
+        "carb": 25.8
+    },
+    {
+        "id": 1779108924896,
+        "nombre": "Empanada Atun (90g/ml)",
+        "kcal": 243.9,
+        "prot": 8.720999999999998,
+        "grasa": 13.671,
+        "carb": 21.06
+    },
+    {
+        "id": 1779108951009,
+        "nombre": "Kéfir Natural 0% (250g/ml)",
+        "kcal": 90,
+        "prot": 9.75,
+        "grasa": 1.25,
+        "carb": 12
+    }
+];
